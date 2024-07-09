bb36c598f8da91d3e829b05cdd04a43c71576cd7
fo <alfonsomartingarcia@gmail.com>
2024-07-09 13:10:50 +0300

    updating landing page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -17,8 +17,32 @@ export const ui = {
         url: "/blog",
       },
     ],
-    mainPageTitle: "Home | The Ultimate Astro Template",
-    mainTitle: "Welcome to the ultimate Astro starter template",
+    mainPageTitle: "Home | Alfonso Martín web developer",
+    portraitAlt: "Alfonso Martín portrait photo",
+    mainTitle: "Alfonso Martín",
+    mainDescription: [
+      {
+        text: "Front-end developer",
+      },
+      {
+        text: "+4 years ",
+      },
+      {
+        text: "experiece.",
+      },
+    ],
+    statSectionItems: [
+      {
+        text: "+20 customers",
+      },
+      {
+        text: "+35 projects",
+      },
+      {
+        text: "+5y experience",
+      },
+    ],
+    mainContact: "Book a call!",
     mainBlogTitle: "Blog | The Ultimate Astro Template",
     blogTitle: "The Blog Page",
   },
@@ -33,8 +57,32 @@ export const ui = {
         url: "/blog",
       },
     ],
-    mainPageTitle: "Inicio | Plantilla definitiva de Astro",
-    mainTitle: "Bievenidos a la plantilla definitiva para proyectos en Astro",
+    mainPageTitle: "Inicio | Alfonso Martín desarrollador web",
+    portraitAlt: "Retrato Alfonso Martín",
+    mainTitle: "Alfonso Martín",
+    mainDescription: [
+      {
+        text: "Desarrollador front-end con ",
+      },
+      {
+        text: "+4 años ",
+      },
+      {
+        text: "de experiencia.",
+      },
+    ],
+    statSectionItems: [
+      {
+        text: "+20 clientes",
+      },
+      {
+        text: "+35 proyectos",
+      },
+      {
+        text: "+5a de esperiencia",
+      },
+    ],
+    mainContact: "Programa una llamada!",
     mainBlogTitle: "Blog | Plantilla definitiva de Astro",
     blogTitle: "El Blog",
   },
